fdd072c1654fca7d3b1d4c813f5eafaf57b25f11
Erin Zylstra <erinzylstra@gmail.com>
2022-05-20 06:58:38 -0700

    Start to summarize events by Park, year

--- data-exploration.R
+++ data-exploration.R
@@ -6,12 +6,33 @@
 # 2022-05-19
 ################################################################################
 
+library(dplyr)
+library(lubridate)
+library(stringr)
+
 #-------------------------------------------------------------------------------#
 # Run script to import, format data
 #-------------------------------------------------------------------------------#
 source("format-mammal-data.R")
 
 # rm(list = ls())
+
+#-------------------------------------------------------------------------------#
+# Visualize when and where cameras were deployed
+#-------------------------------------------------------------------------------#
+table(events$Park, events$d_yr)
+
+events$d_yday <- yday(events$d_date)
+events$r_yday <- yday(events$r_date)
+
+events_py <- as.data.frame(group_by(events, Park, d_yr) %>% 
+  summarize(n_events = length(d_yday), 
+            start_date = min(d_yday),
+            end_date = max(r_yday)))
+
+events_py
+
+
 
 # TO DO:
 # Summarize events - when, length, #detections by species, when detections occur
