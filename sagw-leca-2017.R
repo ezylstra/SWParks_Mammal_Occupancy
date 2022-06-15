1e148bd215c6081afaec8d0a4c8ed3fdf933ceb5
Erin Zylstra <erinzylstra@gmail.com>
2022-06-15 15:06:53 -0700

    Created 2017 detection histories for SAGW, LECA

--- sagw-leca-2017.R
+++ sagw-leca-2017.R
@@ -1,10 +1,13 @@
 ################################################################################
-# SODN -- Camera trap data, 2016-2022
-# Creating detection histories
+# Single-season occupancy analysis
+# SAGW, LECA (black-tailed jackrabbit), 2017
 
 # ER Zylstra
-# 2022-06-02
+# 2022-06-15
 ################################################################################
+
+# Note: Will probably want to create a template script for single-season analyses
+# This will serve as a first case study
 
 library(dplyr)
 library(lubridate)
@@ -13,166 +16,109 @@ library(tidyr)
 
 # rm(list = ls())
 
-#-------------------------------------------------------------------------------#
-# Run script to import, format data
-#-------------------------------------------------------------------------------#
-
+# Load photo observations, camera location info, event matrix, and species list
 source("format-mammal-data.R")
 
-#-------------------------------------------------------------------------------#
-# Subset photo data by species and park
-#-------------------------------------------------------------------------------#
-# Note: will likely want to make this more general (eg, pick > 1 spp)
+# Load sampling occasion data
+occasions <- read.csv("data/occasions/occasions-all-parks.csv")
 
-spp <- "LECA"
-
+# Identify park, species, and year of interest
 park <- "SAGW"
+species <- "LECA"
+year <- 2017
 
-# Subset by species and park, and retain a maximum of one observation per day 
-# at each location
-datsub <- dat %>% 
-  filter(Species_code == spp & Park == park) %>%
-  select(LocationID, StdLocName, obsdate, yr, mon, yday, o_day, POINT_X, POINT_Y) %>%
+# Use day number (day 1 = 01 Jan 2016) for column names in event_mat 
+colnames(event_mat) <- 1:ncol(event_mat)
+
+# Extract sampling occasion info for selected park and year
+occasions <- occasions %>%
+  filter(Park == park & yr == year)
+
+# Select photo observations for park, species and year
+# Retain a maximum of one observation per day at each location
+obs <- dat %>% 
+  filter(Park == park & Species_code == species & yr == year) %>%
+  select(StdLocName, obsdate, yr, o_day) %>%
+  arrange(StdLocName, obsdate) %>%
   distinct
 
-#-------------------------------------------------------------------------------#
-# Define sampling periods, occasions for selected park
-#-------------------------------------------------------------------------------#
-
-# Length of secondary occasions, in days
-occ_length <- 7
-  # May want to evaluate if 7 days is the best choice (Iannarilli et al. 2019?)
-
-# Add colnames to event_mat 
-days_df <- data.frame(daynum = 1:max(dat$o_day), 
-                      date = seq(as.Date("2016-01-01"), max(dat$obsdate), by = 1))
-days_df$yr <- year(days_df$date)
-colnames(event_mat) <- days_df[,1]
-
-# Subset events data
-eventsub <- events %>%
-  filter(Park == park) %>%
-  select(StdLocName, d_date, r_date, d_yr, r_yr, duration, d_day, r_day)
+# Extract information about camera locations in selected park
+locs_park <- locs %>%
+  filter(UnitCode == park) %>%
+  select(StdLocName, POINT_X, POINT_Y) %>%
+  rename(loc = StdLocName, long = POINT_X, lat = POINT_Y)
 
 # Extract rows from events matrix that correspond to locations in selected park
-ddh <- event_mat[rownames(event_mat) %in% eventsub$StdLocName,]
+event_mat <- event_mat[rownames(event_mat) %in% locs_park$loc,]
 
-# Calculate the number of cameras that are deployed each day
-days_df$n_cameras <- colSums(ddh)
-  
-# Calculate proportion of cameras in selected park that are deployed each day
-days_df$prop_deploy <- days_df$n_cameras / nrow(ddh)
+# Convert occasion start/end dates to day numbers
+occasions$start_day <- 
+  as.numeric(date(occasions$start)) - as.numeric(as.Date("2015-12-31"))
+occasions$end_day <- 
+  as.numeric(date(occasions$end)) - as.numeric(as.Date("2015-12-31"))
 
-# Identify those dates when the proportion of cameras deployed >= threshold
-threshold <- 0.60
-days_df$at_thresh <- 1*(days_df$prop_deploy >= threshold)
-  
-# Look at consecutive days with sufficient number of cameras deployed
-data.frame(unclass(rle(days_df$at_thresh)))
-  # filter(days_df, yr == 2017 & prop_deploy > 0)
-
-# Identify the first day in each year that the threshold was met
-firstday_yr <- days_df %>%
-  filter(at_thresh == 1) %>%
-  group_by(yr) %>%
-  summarize(day1 = min(daynum)) %>%
-  as.data.frame
-
-# Generate occasion number (within a year)
-days_df$occasion <- NA
-for (i in days_df$daynum[days_df$at_thresh == 1]) {
-  days_df$occasion[i] <- 
-    ceiling((i - firstday_yr$day1[firstday_yr$yr == days_df$yr[i]] + 1) / occ_length)
+# Create a list of days included in sampling occasions
+occ_days <- NULL
+for (i in 1:nrow(occasions)) {
+  occ_days <- append(occ_days, occasions$start_day[i]:occasions$end_day[i])
 }
 
-# Exclude any occasions shorter than occ_length
-occasions <- days_df %>%
-  filter(!is.na(occasion)) %>%
-  group_by(yr, occasion) %>%
-  summarize(duration = length(occasion),
-            start = min(date),
-            end = max(date)) %>%
-  as.data.frame
-occasions$keep <- 1*(occasions$duration == occ_length) 
-days_df <- left_join(days_df, select(occasions, c(yr, occasion, keep)))
-days_df$keep[is.na(days_df$keep)] <- 0
-  # occasions
-  # days_df[760:800,]
+# Extract columns from events matrix that correspond to sampling occasions
+event_mat <- event_mat[,colnames(event_mat) %in% occ_days]
 
-# Extract just those columns from dh that correspond to sampling occasions
-ddh <- ddh[, colnames(ddh) %in% days_df$daynum[days_df$keep == 1]]
+# Convert the events matrix into detection histories (ddh = daily dh)
+ddh <- event_mat
 
-#-------------------------------------------------------------------------------#
-# Create detection histories 
-#-------------------------------------------------------------------------------#
-
-# Change 0s in dh to NA (NA = camera wasn't operational)
+# Change 0s to NA (NA = camera wasn't deployed)
 ddh[ddh == 0] <- NA
 
-# Change 1s in dh to 0 (0 indicates that the species wasn't detected)
+# Change 1s to 0 (0 indicates that the species wasn't detected)
 ddh[ddh == 1] <- 0
 
 # Replace 0s with 1s when the species was detected
-for (i in 1:nrow(datsub)) {
-  ddh[rownames(ddh) == datsub$StdLocName[i], 
-     colnames(ddh) == as.character(datsub$o_day[i])] <- 1
+for (i in 1:nrow(obs)) {
+  ddh[rownames(ddh) == obs$StdLocName[i], 
+      colnames(ddh) == as.character(obs$o_day[i])] <- 1
 }
 # checks:
-table(ddh)
-days_df[days_df$daynum %in% datsub$o_day[!datsub$o_day %in% colnames(ddh)],]
-  # Detections that aren't included in detection histories are either:
-    # on days when few cameras were deployed (< threshold)
-    # or, during occasions that were shorter than occ_length
+sum(ddh == 1, na.rm = TRUE)
+sum(obs$o_day %in% occ_days)
 
 # Create a function to aggregate daily detection data during each occasion
-  # NA if the camera was not operational during entire occasion (all values = NA)
+  # NA if the camera was not operational throughout entire occasion (all values = NA)
   # 1 if species was detected one or more times (regardless if there are NAs)
   # 0 if species was never detected
   paNA <- function(x) {
     if (sum(is.na(x)) == length(x)) {NA} else 
       if (sum(x, na.rm = TRUE) == 0) {0} else {1} 
   }
+  
+# Create a function to calculate the proportion of a sampling period a camera
+# was operational
+  propNA <- function(x) {
+    (occasions$duration[1] - sum(is.na(x))) / occasions$duration[1]
+  }
 
-# Summarize detection data (dh) and effort (eff) during each occasion 
-occ_starts <- seq(1, ncol(ddh), by = occ_length)
-occ_ends <- occ_starts + occ_length - 1
-dh <- eff <-  matrix(NA, 
-                     nrow = nrow(ddh), 
-                     ncol = ncol(ddh) / occ_length,
-                     dimnames = list(rownames(ddh), NULL))
+# Summarize detection data (dh) and effort during each occasion 
+dh <- effort <-  matrix(NA, 
+                        nrow = nrow(ddh), 
+                        ncol = ncol(ddh) / occasions$duration[1],
+                        dimnames = list(rownames(ddh), NULL))
+
 for (i in 1:ncol(dh)) {
-  multiday <- ddh[,occ_starts[i]:occ_ends[i]]
+  multiday <- ddh[,colnames(ddh) %in% occasions$start_day[i]:occasions$end_day[i]]
   dh[,i] <- apply(multiday, 1, paNA)
-  eff[,i] <- apply(multiday, 1, function(x) {(occ_length - sum(is.na(x))) / occ_length})
+  effort[,i] <- apply(multiday, 1, propNA)
 }
-
-#-------------------------------------------------------------------------------#
-# Temporal covariates
-#-------------------------------------------------------------------------------#
-# Will expand this section as more covariates become available
-
-temporal_covs <- occasions %>%
-  filter(keep == 1) %>%
-  select(-c(duration, keep)) %>%
-  rowwise %>%
-  mutate(mid = mean.Date(c(start,end)),
-         y2018 = 1*(yr == 2018),
-         y2020 = 1*(yr == 2020),
-         y2021 = 1*(yr == 2021),
-         y2022 = 1*(yr == 2022),
-         trend = yr - 2017) %>%
-  as.data.frame
+  # Might need to replace 0 values in effort with NA
 
 #-------------------------------------------------------------------------------#
 # Spatial covariates
 #-------------------------------------------------------------------------------#
 # Will expand this section as more covariates become available
 
-spatial_covs <- locs %>%
-  filter(UnitCode == park) %>%
-  select(UnitCode, StdLocName, POINT_X, POINT_Y) %>%
-  rename(park = UnitCode, loc = StdLocName, long = POINT_X, lat = POINT_Y)
+spatial_covs <- locs_park 
 
 # Ensure the order is the same as what's in the detection history matrix
-spatial_covs <- spatial_covs[match(rownames(ddh), spatial_covs$loc),]
+spatial_covs <- spatial_covs[match(rownames(dh), spatial_covs$loc),]
 
