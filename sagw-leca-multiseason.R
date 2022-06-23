89e3776c26c26995555984465ce04606ee52ca24
Erin Zylstra <erinzylstra@gmail.com>
2022-06-23 15:45:36 -0700

    Start example script for a multi-season analysis

---
+++ sagw-leca-multiseason.R
@@ -0,0 +1,166 @@
+################################################################################
+# Multi-season occupancy analysis
+# SAGW, LECA (black-tailed jackrabbit)
+
+# ER Zylstra
+# 2022-06-23
+################################################################################
+
+# Note: Will probably want to create a template script for this type of analysis
+# This will serve as a first case study
+
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(tidyr)
+library(jagsUI)
+
+# rm(list = ls())
+
+# Load photo, location, events, species data 
+source("format-mammal-data.R")
+
+  # dat = information about each photo (date, time, species, location)
+  # events = information about each camera deployment (dates, location, duration)
+  # event_mat = camera location x day matrix with 1/0 indicating whether camera
+  #             was deployed or not
+  # locs = information about each camera location (park, lat/long, name)
+  # species = table with species observed (species code, common name, # of obs)
+
+# Load sampling occasion data (park, year, start/end, duration)
+occasions <- read.csv("data/occasions/occasions-all-parks.csv")
+
+# Identify park, species, and year of interest
+park <- "SAGW"
+species <- "LECA"
+
+# Use day number (day 1 = 01 Jan 2016) for column names in event_mat 
+colnames(event_mat) <- 1:ncol(event_mat)
+
+#-------------------------------------------------------------------------------#
+# Create detection histories for selected park, species
+#-------------------------------------------------------------------------------#
+
+# Extract sampling occasion info for selected park
+occasions <- occasions %>%
+  filter(Park == park)
+
+# Add occasion ID
+occasions$yr_occ <- paste0(occasions$yr, "_", occasions$occasion)
+
+# Convert occasion start/end dates to day numbers
+occasions$start_day <- 
+  as.numeric(date(occasions$start)) - as.numeric(as.Date("2015-12-31"))
+occasions$end_day <- 
+  as.numeric(date(occasions$end)) - as.numeric(as.Date("2015-12-31"))
+
+# Create a list of days included in sampling occasions
+occ_days <- NULL
+for (i in 1:nrow(occasions)) {
+  occ_days <- append(occ_days, occasions$start_day[i]:occasions$end_day[i])
+}
+
+# Extract photo observations for park, species
+# Retain a maximum of one observation per day at each location
+obs <- dat %>% 
+  filter(Park == park & Species_code == species) %>%
+  select(StdLocName, obsdate, yr, o_day) %>%
+  arrange(StdLocName, obsdate) %>%
+  distinct
+
+# Extract information about camera locations in selected park
+locs_park <- locs %>%
+  filter(UnitCode == park) %>%
+  select(StdLocName, POINT_X, POINT_Y) %>%
+  rename(loc = StdLocName, long = POINT_X, lat = POINT_Y)
+
+# Extract rows from events matrix that correspond to locations in selected park
+event_mat <- event_mat[rownames(event_mat) %in% locs_park$loc,]
+
+# Extract columns from events matrix that correspond to sampling occasions
+event_mat <- event_mat[,colnames(event_mat) %in% occ_days]
+
+# Convert the events matrix into daily detection histories (ddh)
+ddh <- event_mat
+
+# Change 0s to NA (NA = camera wasn't deployed)
+ddh[ddh == 0] <- NA
+
+# Change 1s to 0 (0 indicates that the species wasn't detected)
+ddh[ddh == 1] <- 0
+
+# Replace 0s with 1s when the species was detected
+for (i in 1:nrow(obs)) {
+  ddh[rownames(ddh) == obs$StdLocName[i], 
+      colnames(ddh) == as.character(obs$o_day[i])] <- 1
+}
+# checks:
+sum(ddh == 1, na.rm = TRUE)
+sum(obs$o_day %in% occ_days)
+
+# Create a function to aggregate daily detection data during each occasion
+  # NA if camera wasn't operational throughout entire occasion (all values = NA)
+  # 1 if species was detected one or more times (even if there are NAs)
+  # 0 if species was never detected
+paNA <- function(x) {
+  if (sum(is.na(x)) == length(x)) {NA} else 
+    if (sum(x, na.rm = TRUE) == 0) {0} else {1} 
+}
+
+# Create a function to calculate the proportion of a sampling period a camera
+# was operational
+propNA <- function(x) {
+  (occasions$duration[1] - sum(is.na(x))) / occasions$duration[1]
+}
+
+# Summarize detection data (dh) and effort during each occasion 
+dh <- effort <- matrix(NA, 
+                       nrow = nrow(ddh), 
+                       ncol = ncol(ddh) / occasions$duration[1],
+                       dimnames = list(rownames(ddh), NULL))
+
+for (i in 1:ncol(dh)) {
+  multiday <- ddh[,colnames(ddh) %in% occasions$start_day[i]:occasions$end_day[i]]
+  dh[,i] <- apply(multiday, 1, paNA)
+  effort[,i] <- apply(multiday, 1, propNA)
+}
+
+# In contrast to our single-season model, we'll put detection/survey data in 
+# long form. This makes it easier to create universal model scripts that can
+# be used with different combinations of covariates, and sometimes this
+# results in shorter run times. 
+
+# Basically, instead of having site * occasion matrices, we'll 
+# create long vectors with detection and effort data, along with accompanying 
+# variables to indicate the site, location, and year for the given observations.
+
+# Convert detection data into long form
+dh_df <- as.data.frame(dh)
+colnames(dh_df) <- sort(occasions$yr_occ)
+dh_df$loc <- row.names(dh_df)
+dh_long <- dh_df %>%
+  pivot_longer(!loc,
+               names_to = "occ",
+               values_to = "det") %>%
+  arrange(loc, occ) %>%
+  as.data.frame
+
+# Convert effort data into long form
+eff_df <- as.data.frame(effort)
+colnames(eff_df) <- sort(occasions$yr_occ)
+eff_df$loc <- row.names(eff_df)
+eff_long <- eff_df %>%
+  pivot_longer(!loc,
+               names_to = "occ",
+               values_to = "effort") %>%
+  arrange(loc, occ) %>%
+  as.data.frame
+
+# Merge detection and effort data
+surveys <- left_join(dh_long, eff_long)
+  # Count unique combinations of detection, effort data
+  count(surveys, det, effort) 
+  
+# Add year and trend columns
+surveys$yr <- as.numeric(str_sub(surveys$occ, 1,4))
+surveys$trend <- surveys$yr - min(surveys$yr)
