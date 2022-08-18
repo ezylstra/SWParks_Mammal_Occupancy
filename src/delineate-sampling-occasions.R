723416d248f195ac28cf21e6f779523b0beb6e27
Erin Zylstra <erinzylstra@gmail.com>
2022-08-17 17:09:23 -0700

    Fix error with last possible date

--- src/delineate-sampling-occasions.R
+++ src/delineate-sampling-occasions.R
@@ -24,12 +24,11 @@ source("src/format-mammal-data.R")
 
 # Create a dataframe with information about each day of the study
 # Day number (daynum): day 1 = 01 Jan 2016)
-days_df <- data.frame(daynum = 1:max(dat$o_day), 
-                      date = seq(as.Date("2016-01-01"), max(dat$obsdate), by = 1))
+days_df <- data.frame(daynum = 1:max(events$r_day), 
+                      date = seq(as.Date("2016-01-01"), 
+                                 max(events$r_date), 
+                                 by = 1))
 days_df$yr <- year(days_df$date)
-
-# Use day number (day 1 = 01 Jan 2016) for column names in event_mat 
-colnames(event_mat) <- days_df[,1]
 
 # Set the length of sampling occasions, in days
 occ_length <- 7
