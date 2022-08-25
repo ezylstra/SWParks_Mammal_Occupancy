5be15a645972a851182e1c1382b0604085b41bd3
Erin Zylstra <erinzylstra@gmail.com>
2022-08-25 15:44:27 -0700

    Start exploring covariate data, esp precip

---
+++ src/covariate-data-exploration.R
@@ -0,0 +1,55 @@
+################################################################################
+# SODN -- Camera trap data, 2016-2022
+# Initial exploration of covariate data
+
+# ER Zylstra
+# 2022-08-25
+################################################################################
+
+library(dplyr)
+library(terra)
+
+# Load park boundaries
+parks <- vect("data/covariates/SWNC_nps_boundary_22020630.shp")
+
+# Load park boundaries + 3-km buffer
+parks_b <- vect("data/covariates/SWNC_nps_boundary_3km_22020630.shp")
+
+# Looks like the park boundary/buffer layers have 3 polygons for Saguaro NP 
+# (SAGU, SAGW, SAGE) that are slightly offset. 
+# For now, we'll just extract the SAGW polygon, plus CHIR, ORPI
+parks <- subset(parks, parks$UNIT_CODE %in% c("CHIR", "ORPI", "SAGW"))
+parks_b <- subset(parks_b, parks_b$UNIT_CODE %in% c("CHIR", "ORPI", "SAGW"))
+
+# Load 30-m DEMs for each park
+dem_chir <- rast("data/covariates/CHIR_DEM_1as.tif")
+dem_orpi <- rast("data/covariates/ORPI_DEM_1as.tif")
+dem_sagw <- rast("data/covariates/SAGW_DEM_1as.tif")
+
+# Load precipitation data
+# (later I can automate this with apply/loops)
+pr2016 <- rast("data/covariates/pr2016.nc")
+pr2017 <- rast("data/covariates/pr2017.nc")
+pr2018 <- rast("data/covariates/pr2018.nc")
+pr2019 <- rast("data/covariates/pr2019.nc")
+pr2020 <- rast("data/covariates/pr2020.nc")
+pr2021 <- rast("data/covariates/pr2021.nc")
+pr2022 <- rast("data/covariates/pr2022.nc")
+
+# check:
+plot(pr2016[[365]])
+plot(parks, add = TRUE)
+plot(parks_b, lty = 3, add = TRUE)
+
+# TODO with precip data
+  # Convert layer name to date: 
+    # see https://tmieno2.github.io/R-as-GIS-for-Economists/gridMET.html
+  # Sum values across days of interest
+  # Cold and warm seasons that NPS has used for other projects?
+  # Monsoon precip (15 Jun - 30 Sep)
+  # Winter precip? But note that cameras deployed early in the year.
+  # For winter we'll need to combine information from 2 years
+
+
+
+
