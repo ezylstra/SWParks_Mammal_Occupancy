c8f085755880b249cdaf2337a6858d83d9bd0d90
Erin Zylstra <erinzylstra@gmail.com>
2023-06-22 16:00:20 -0700

    Add overall occ/det probabilibities for each spp

--- src/run-single-season-report.R
+++ src/run-single-season-report.R
@@ -1,11 +1,16 @@
-# Render test
+# Create single-season report for a specified park and year
 
 park <- "SAGW"
 year <- 2022
 
-rmarkdown::render("src/single-season-report.Rmd",
-                  params = list(PARK = park,
-                                YEAR = year,
-                                date = Sys.Date()),
-                  output_dir = paste0(getwd(), "/output/single-season-reports/"), 
-                  output_file = paste0(park, "-", year))
+suppressWarnings(
+  rmarkdown::render("src/single-season-report.Rmd",
+                    params = list(PARK = park,
+                                  YEAR = year,
+                                  date = Sys.Date()),
+                    output_file = paste0("../output/single-season-reports/", 
+                                         park, "-", year))
+)
+
+# Note that the default directory for the output file is where the input 
+# file is located (here, src/)
