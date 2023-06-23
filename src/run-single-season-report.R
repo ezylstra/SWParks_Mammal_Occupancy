82635a8362e3c0c64c7826f146cfd05158c306a9
Erin Zylstra <erinzylstra@gmail.com>
2023-06-23 13:45:37 -0700

    Add info about input/output files

--- src/run-single-season-report.R
+++ src/run-single-season-report.R
@@ -3,6 +3,13 @@
 park <- "SAGW"
 year <- 2022
 
+# For this to work, models for one or more species in the specified park 
+# and year need to be saved here: output/single-single-models/
+# Files should be named: PARK-YEAR-SPECIES.rds
+
+# The resulting word document will be saved here: output/single-season-reports/
+# File will be named: PARK-YEAR.docx
+
 suppressWarnings(
   rmarkdown::render("src/single-season-report.Rmd",
                     params = list(PARK = park,
@@ -12,5 +19,5 @@ suppressWarnings(
                                          park, "-", year))
 )
 
-# Note that the default directory for the output file is where the input 
-# file is located (here, src/)
+# Note that for render(), the default directory for the output file is where the 
+# input file is located (here, src/)
